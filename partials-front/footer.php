dd5974ca2aec2ff22f41e7658ac95319f268588a
Satyam <70771653+Satyam-79@users.noreply.github.com>
2023-07-29 17:19:48 +0530

    bug

--- partials-front/footer.php
+++ partials-front/footer.php
@@ -16,13 +16,5 @@
     </section>
     <!-- social Section Ends Here -->
 
-    <!-- footer Section Starts Here -->
-    <section class="footer">
-        <div class="container text-center">
-            <p>All rights reserved. Designed By <a href="#">Vijay Thapa</a></p>
-        </div>
-    </section>
-    <!-- footer Section Ends Here -->
-
 </body>
 </html>
