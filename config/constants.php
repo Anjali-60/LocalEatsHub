c035057a7b154b40fc5a9712d344769d38351dbc
Satyam-79 <satyamverma79@gmail.com>
2023-07-30 12:03:09 +0530

    bug fix

--- config/constants.php
+++ config/constants.php
@@ -4,11 +4,11 @@
 
 
     //Create Constants to Store Non Repeating Values
-    define('SITEURL', 'http://localhost/food-order/');
+    define('SITEURL', 'http://localhost/LocalEatsHub/');
     define('LOCALHOST', 'localhost');
     define('DB_USERNAME', 'root');
     define('DB_PASSWORD', '');
-    define('DB_NAME', 'food-order');
+    define('DB_NAME', 'localeatshub');
     
     $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or die(mysqli_error()); //Database Connection
     $db_select = mysqli_select_db($conn, DB_NAME) or die(mysqli_error()); //SElecting Database
--- database/localeatshub.sql
+++ database/localeatshub.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 29, 2023 at 01:53 PM
+-- Generation Time: Jul 30, 2023 at 08:30 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `food-order`
+-- Database: `localeatshub`
 --
 
 -- --------------------------------------------------------
